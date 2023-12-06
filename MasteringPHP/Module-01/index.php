5b5e6e8a055f94f0d57183dafade4f0cb59ba3aa
Mahabubur Rahman <80971191+DigitalMarketerMahabub@users.noreply.github.com>
2023-12-06 18:16:50 +0600

    php learning module01

---
+++ MasteringPHP/Module-01/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mastering PHP</title>
+</head>
+<body>
+<!-- Task 1 -->
+    <h1>Hello Programmers</h1>
+    <p>Let's learn to php with my fabulous teammate</p>
+
+    <?php
+    echo "<h1> hello world </h1>";
+    ?>
+
+<!-- Task 2 -->
+    <?php
+    $studentname = "Mahabubur Masum";
+    $coursename = "Mastering in php & MySQL";
+    ?>
+
+    <h1>Hello team members, I am <?php echo $studentname; ?>. this is our first project</h1>
+    <p>Our course name is <?php echo $coursename; ?>. We want to caring about this course, if we don't take serious can't learning. thank you all</p>
+
+<!-- Task 3 -->
+</body>
+</html>
